682d7e763ce686e0e5591d6e2d85cbed2d174076
Caner Hazirbas <hazirbas@cs.tum.edu>
2016-08-08 11:18:15 +0200

    checkerboard detection code by the developer of the toolbox: Donald Dansereau

--- LFToolbox0.4/SupportFunctions/LFCalInit.m
+++ LFToolbox0.4/SupportFunctions/LFCalInit.m
@@ -92,13 +92,38 @@ for( iFile = 1:length(CalOptions.FileList) )
                 ValidCheckerCount = ValidCheckerCount + 1;
                 PerImageValidCount = PerImageValidCount + 1;
                 
+%                 %--- reorient to expected ---
+%                 Centroid = mean( CurChecker,2 );
+%                 IsTopLeft = all(CurChecker(:,1) < Centroid);
+%                 IsTopRight = (CurChecker(1,1) > Centroid(1) && CurChecker(2,1) < Centroid(2));
+%                 if( IsTopRight )
+%                     CurChecker = reshape(CurChecker, [2, CalOptions.ExpectedCheckerSize]);
+%                     CurChecker = CurChecker(:, end:-1:1, :);
+%                     CurChecker = permute(CurChecker, [1,3,2]);
+%                     CurChecker = reshape(CurChecker, 2, []);
+%                 end
+%                 IsTopLeft = all(CurChecker(:,1) < Centroid);
+%                 assert( IsTopLeft, 'Error: unexpected point order from detectCheckerboardPoints' );
+
                 %--- reorient to expected ---
                 Centroid = mean( CurChecker,2 );
                 IsTopLeft = all(CurChecker(:,1) < Centroid);
                 IsTopRight = (CurChecker(1,1) > Centroid(1) && CurChecker(2,1) < Centroid(2));
+                IsBotLeft = (CurChecker(1,1) < Centroid(1) && CurChecker(2,1) > Centroid(2));
+                IsBotRight = all(CurChecker(:,1) > Centroid);
                 if( IsTopRight )
                     CurChecker = reshape(CurChecker, [2, CalOptions.ExpectedCheckerSize]);
                     CurChecker = CurChecker(:, end:-1:1, :);
+                    CurChecker = permute(CurChecker, [1,3,2]);
+                    CurChecker = reshape(CurChecker, 2, []);
+                elseif( IsBotLeft )
+                    CurChecker = reshape(CurChecker, [2, CalOptions.ExpectedCheckerSize]);
+                    CurChecker = CurChecker(:, :, end:-1:1);
+                    CurChecker = permute(CurChecker, [1,3,2]);
+                    CurChecker = reshape(CurChecker, 2, []);
+                elseif( IsBotRight ) % untested case
+                    CurChecker = reshape(CurChecker, [2, CalOptions.ExpectedCheckerSize]);
+                    CurChecker = CurChecker(:, end:-1:1, end:-1:1);
                     CurChecker = permute(CurChecker, [1,3,2]);
                     CurChecker = reshape(CurChecker, 2, []);
                 end
